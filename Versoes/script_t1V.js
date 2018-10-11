3a2df91e5d3f581966c515ef4362fc9ac5dac82f
Beatriz Alexandre <beatrizga@al.insper.edu.br>
2018-10-11 14:18:21 -0300

    APS 4 pronta

--- Versoes/script_t1V.js
+++ Versoes/script_t1V.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		})
 		
+
 	})
 
     db.download(url, function(data) {
@@ -25,6 +26,41 @@ document.addEventListener('DOMContentLoaded', function() {
     		'courses': data,
 
     	})
+
+    /*
+        Inicializa uma medição de tempo, que será gravada no
+        banco de dados cujo endereço é passado como parâmetro.
+        Altere o endereço abaixo para seu próprio banco de dados.
+    */
+    let ab = start('https://teste-a-e-b.firebaseio.com/')
+
+    /*
+        Seleciona o elemento que, quando clicado, finaliza a
+        medição de tempo e envia o resultado para o banco.
+        Altere o seletor abaixo para o do elemento desejado.
+    */
+    let element = document.querySelector('.continue')
+
+    /*
+        A princípio, o restante do código não precisa mudar.
+    */
+    
+    element.addEventListener('click', function() {
+        ab.finish()
+    })
+
+    let as = document.querySelectorAll('a')
+
+    for(let a of as) {
+        a.addEventListener('click', function(event) {
+            event.preventDefault()
+            setTimeout(function() {
+                window.location.assign(a.href)
+            }, 5000)
+        })
+    }
+
+
 
     })
 /*
